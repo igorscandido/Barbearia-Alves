da54339a77350a5c2aac2228be47360705ab6664
igorcandido <igorcandido@proton.me>
2025-05-25 22:42:53 -0300

    feat(seeders): add database seeders for initial data population

---
+++ database/seeders/DatabaseSeeder.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        \App\Models\User::create([
+            'name' => 'Administrador',
+            'email' => 'admin@barbearia.com',
+            'password' => Hash::make('admin1234'),
+            'role' => 'admin',
+        ]);
+        \App\Models\User::create([
+            'name' => 'Cliente',
+            'email' => 'cliente@barbearia.com',
+            'password' => Hash::make('cliente1234'),
+            'role' => 'cliente',
+        ]);
+        \App\Models\User::create([
+            'name' => 'Barbeiro Daniel',
+            'email' => 'barbeiro@barbearia.com',
+            'password' => Hash::make('barbeiro1234'),
+            'role' => 'barbeiro',
+        ]);
+    }
+}
